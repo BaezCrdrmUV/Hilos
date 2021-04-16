9ec0a87fca086ad643e6e676dba7031e33029917
BaezCrdrm <dev.baez_crdrm@outlook.com>
2021-04-16 16:15:48 -0500

    Multihilo

--- Program.cs
+++ Program.cs
@@ -5,9 +5,35 @@ namespace Threads
 {
     class Program
     {
+        static int total = 100;
         static void Main(string[] args)
         {
-            MonoCount();
+            // Validación y asignación de número personalizado de vueltas
+            if(args.Length > 1 && String.IsNullOrEmpty(args[1]))
+            {
+                try
+                {
+                    total = Int32.Parse(args[1]);
+                }
+                catch (System.Exception) { }
+            }
+
+            if(args.Length == 0 || args[0] == "mono")
+            {
+                MonoCount("1MonoCount1", total);
+                MonoCount("2MonoCount2", total, 200);
+            }
+            else if(args[0] == "multi")
+            {
+                // Nuevo hilo
+                Thread thread = new Thread(new ThreadStart(MultiCount));
+                thread.Start();
+
+                // Hilo principal
+                MonoCount("1MonoCount1", total, 100);
+                // Sincronización
+                thread.Join();
+            }
         }
 
         public static void MonoCount(string name = "thread", int loops = 100, int millisecond = 500)
@@ -17,6 +43,11 @@ namespace Threads
                 Console.WriteLine("{0} L-{1}", name, i);
                 Thread.Sleep(millisecond);
             }
+        }
+
+        public static void MultiCount()
+        {
+            MonoCount("Multi-Count", total, 700);
         }
     }
 }
