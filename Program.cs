713999451866995ed41e4d326c0b34e6b3906f50
BaezCrdrm <dev.baez_crdrm@outlook.com>
2021-04-16 15:52:03 -0500

    Mono hilo

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace Threads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MonoCount();
+        }
+
+        public static void MonoCount(string name = "thread", int loops = 100, int millisecond = 500)
+        {
+            for (int i = 0; i < loops; i++)
+            {
+                Console.WriteLine("{0} L-{1}", name, i);
+                Thread.Sleep(millisecond);
+            }
+        }
+    }
+}
